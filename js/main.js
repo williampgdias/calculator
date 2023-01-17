884c83ccc9edec1d1064f1b273c2de77ff65854b
William Dias <smdwilliam89@gmail.com>
2023-01-17 11:13:34 +0000

    doing JS again

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+// Função para calculadora funcionar
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == 'clear') {
+      display.innerText = '';
+    } else if (item.id == 'backspace') {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != '' && item.id == 'equal') {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == '' && item.id == 'equal') {
+      display.innerText = 'Empty!';
+      setTimeout(() => (display.innerText = ''), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+// Função para mudar o tema escuro/claro
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+const toggleIcon = document.querySelector('.toggler-icon');
+let isDark = true;
+
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle('dark');
+  themeToggleBtn.classList.toggle('active');
+  isDark = !isDark;
+};
